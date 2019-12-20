d59decbc808b54cde13b507b0474951922425007
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2019-12-20 16:46:26 -0300

    Adicionando rotas de detalhe, Put e Delete

--- node-api-test/src/controllers/ProductController.js
+++ node-api-test/src/controllers/ProductController.js
@@ -2,13 +2,28 @@ const mongoose = require('mongoose')
 const Product = mongoose.model('Product')
 
 module.exports = {
-    async index(req, res) {
+    async listar(req, res) {
         const products = await Product.find()
         return res.json(products)
     },
 
-    async store(req, res) {
+    async details(req, res) {
+        const product = await Product.findById(req.params.id)
+        return res.json(product)
+    },
+
+    async incluir(req, res) {
         const product = await Product.create(req.body)
         return res.json(product)
-    }
+    },
+
+    async alterar(req, res) {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new:true})
+        return res.json(product)
+    },
+
+    async deletar(req, res) {
+        await Product.findByIdAndRemove(req.params.id)
+        return res.send('Item Removido com sucesso')
+    },
 }
--- node-api-test/src/routes.js
+++ node-api-test/src/routes.js
@@ -4,7 +4,10 @@ const routes = express.Router()
 const ProductController = require('./controllers/ProductController')
 
 // definição das rotas da api
-routes.get('/products', ProductController.index)
-routes.post('/products', ProductController.store)
+routes.get('/products', ProductController.listar)
+routes.get('/products/:id', ProductController.details)
+routes.post('/products', ProductController.incluir)
+routes.put('/products/:id', ProductController.alterar)
+routes.delete('/products/:id', ProductController.deletar)
 
 module.exports = routes;
