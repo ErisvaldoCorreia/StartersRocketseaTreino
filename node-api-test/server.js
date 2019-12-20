4a901d61f06bd826bf8aaf27c7a23a135d1f9d3e
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2019-12-20 19:58:05 -0300

    Paginação de listagem e Adição de Cors

--- node-api-test/server.js
+++ node-api-test/server.js
@@ -1,10 +1,13 @@
 const express = require('express')
+// import cors para permissão de acesso externo a api
+const cors = require('cors')
 const mongoose = require('mongoose')
 // importando biblioteca para conectar diretório de models
 const requireDir = require('require-dir')
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 mongoose.connect(
     'mongodb://localhost:27017/nodeapitest', 
